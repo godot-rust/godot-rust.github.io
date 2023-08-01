ae3f8ae8fe2b0ac9d7b73ac8d14f78c978fec324
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2023-08-01 08:52:16 +0000

    Update gdext master (`02823b4e8`)
                Commit:       https://www.github.com/godot-rust/gdext/commit/02823b4e80b126a84a9daaec93fe4d60338a8caa
                Triggered at: 2023-08-01T08:40:41Z

--- docs/gdext/master/godot/obj/sidebar-items.js
+++ docs/gdext/master/godot/obj/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["cap","dom","mem"],"struct":["Base","Gd","GdMut","GdRef","InstanceId"],"trait":["EngineClass","EngineEnum","ExportableObject","GodotClass","Inherits","Share"]};
+window.SIDEBAR_ITEMS = {"mod":["cap","dom","mem"],"struct":["Base","Gd","GdMut","GdRef","InstanceId"],"trait":["EngineClass","EngineEnum","ExportableObject","GodotClass","IndexEnum","Inherits","Share"]};
--- website-docs-md/gdext-master.md
+++ website-docs-md/gdext-master.md
@@ -8,8 +8,8 @@ docs_rel_url = "gdext/master/godot"
 pr_url = "https://github.com/godot-rust/gdext"
 pr_author = ""
 sort_key = 9999-01-01
-date_time = 2023-08-01T05:58:30Z
-commit_sha = "157afcf2f16eef1ba8272c0d044d92c03e14c986"
+date_time = 2023-08-01T08:40:41Z
+commit_sha = "02823b4e80b126a84a9daaec93fe4d60338a8caa"
 +++
 
 
