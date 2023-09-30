b554853f41c382647f9eef4b939d3b91d25b68bb
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2023-09-30 08:43:11 +0000

    Remove gdext PR #433
            Pull request: https://www.github.com/godot-rust/gdext/pull/433
            Triggered at: 2023-09-30T08:40:43Z

--- docs/gdext/pr-433/godot/engine/tile_map/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["ExForceUpdate","ExGetCellAlternativeTile","ExGetCellAtlasCoords","ExGetCellSourceId","ExGetCellTileData","ExGetUsedCellsById","ExSetCell","ExSetCellsTerrainConnect","ExSetCellsTerrainPath","VisibilityMode"]};
