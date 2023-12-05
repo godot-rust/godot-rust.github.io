788a513ab94979417af8dbd0c6e06ab245319796
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2023-12-05 12:08:50 +0000

    Remove gdext PR #525
            Pull request: https://www.github.com/godot-rust/gdext/pull/525
            Triggered at: 2023-12-05T12:07:17Z

--- docs/gdext/pr-525/godot/engine/a_star_grid_2d/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["DiagonalMode","ExFillSolidRegion","ExSetPointSolid","Heuristic"]};
--- docs/gdext/pr-525/godot/prelude/meta/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["registration"],"struct":["ClassName","ConvertError","PropertyInfo"],"trait":["FromGodot","GodotConvert","GodotType","ToGodot"]};
