c4ba22d903dc6e480f7284d0c31d7888bb90e079
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2023-12-12 16:47:03 +0000

    Remove gdext PR #534
            Pull request: https://www.github.com/godot-rust/gdext/pull/534
            Triggered at: 2023-12-12T16:45:39Z

--- docs/gdext/pr-534/godot/obj/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["cap","dom","mem"],"struct":["Base","Gd","GdMut","GdRef","InstanceId","OnReady"],"trait":["EngineBitfield","EngineClass","EngineEnum","ExportableObject","GodotClass","IndexEnum","Inherits","Share","UserClass","WithBaseField"]};
