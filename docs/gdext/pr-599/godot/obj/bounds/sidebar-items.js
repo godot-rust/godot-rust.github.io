387897eb88d136a99230888402bd350708a0bdf8
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-02-11 12:01:09 +0000

    Remove gdext PR #599
            Pull request: https://www.github.com/godot-rust/gdext/pull/599
            Triggered at: 2024-02-11T11:59:38Z

--- docs/gdext/pr-599/godot/obj/bounds/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["DeclEngine","DeclUser"],"macro":["implement_godot_bounds"],"struct":["MemDynamic","MemManual","MemRefCounted"],"trait":["Declarer","DynMemory","Memory"]};
