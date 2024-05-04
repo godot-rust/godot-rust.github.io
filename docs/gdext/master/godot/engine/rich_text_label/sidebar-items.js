51fa6c688a1a2f9948196b95b8cfaa91dbc61908
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-05-04 17:55:43 +0000

    Update gdext master (`cadec6b260`)
                Commit:       https://www.github.com/godot-rust/gdext/commit/cadec6b2602f1fc1ff1a5033bfe2477595dd09ed
                Triggered at: 2024-05-04T17:45:04Z

--- docs/gdext/master/godot/engine/rich_text_label/sidebar-items.js
+++ docs/gdext/master/godot/engine/rich_text_label/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["ExAddImage","ExPushDropcap","ExPushFont","ExPushList","ExPushMeta","ExPushParagraph","ExPushTable","ExSetTableColumnExpand","ExUpdateImage","ImageUpdateMask","ListType","MenuItems","MetaUnderline"]};
+window.SIDEBAR_ITEMS = {"struct":["ExAddImage","ExPushDropcap","ExPushFont","ExPushList","ExPushMeta","ExPushParagraph","ExPushTable","ExRemoveParagraph","ExSetTableColumnExpand","ExUpdateImage","ImageUpdateMask","ListType","MenuItems","MetaUnderline"]};
--- website-docs-md/gdext-master.md
+++ website-docs-md/gdext-master.md
@@ -1,6 +1,6 @@
 +++
 title = "master"
-date = 2024-05-01
+date = 2024-05-04
 
 [extra]
 repo = "gdext"
@@ -8,8 +8,8 @@ docs_rel_url = "gdext/master/godot"
 pr_url = "https://github.com/godot-rust/gdext"
 pr_author = ""
 sort_key = 9999-01-01
-date_time = 2024-05-01T20:14:50Z
-commit_sha = "cb41673a0ab802145543a8f371abfcb1f0be10f9"
+date_time = 2024-05-04T17:45:04Z
+commit_sha = "cadec6b2602f1fc1ff1a5033bfe2477595dd09ed"
 +++
 
 
