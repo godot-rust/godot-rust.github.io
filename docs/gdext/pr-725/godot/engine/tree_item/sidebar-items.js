e8e179072550c83b476a2ac55344f71f58558253
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-05-25 12:20:51 +0000

    Remove gdext PR #725
            Pull request: https://www.github.com/godot-rust/gdext/pull/725
            Triggered at: 2024-05-25T12:18:34Z

--- docs/gdext/pr-725/godot/engine/tree_item/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["ExAddButton","ExCreateChild","ExGetNextInTree","ExGetNextVisible","ExGetPrevInTree","ExGetPrevVisible","ExIsAnyCollapsed","ExPropagateCheck","ExSetCustomBgColor","ExSetRangeConfig","TreeCellMode"]};
