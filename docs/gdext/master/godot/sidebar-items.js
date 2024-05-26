a981242ade611b25961e6dd475b88217c01b492c
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-05-26 17:50:21 +0000

    Update gdext master (`4f9f47f8ba`)
                Commit:       https://www.github.com/godot-rust/gdext/commit/4f9f47f8ba6a9f0103bab62af5436f96244cbad8
                Triggered at: 2024-05-26T17:39:17Z

--- docs/gdext/master/godot/sidebar-items.js
+++ docs/gdext/master/godot/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["builtin","engine","init","log","obj","prelude","register"]};
+window.SIDEBAR_ITEMS = {"mod":["builtin","engine","global","init","log","obj","prelude","register"]};
