a809f8621e03162264cd4a14a2a8c53809c25bdc
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-06-23 13:18:46 +0000

    Create gdext PR #776 (`7a6fdfebe3`)
                Pull request: https://www.github.com/godot-rust/gdext/pull/776
                Commit:       https://www.github.com/godot-rust/gdext/pull/776/commits/7a6fdfebe3454abdf09436c97067dcf2e34861ba
                Triggered at: 2024-06-23T13:14:51Z

---
+++ docs/gdext/pr-776/godot/classes/input_event/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ExGetActionStrength","ExIsAction","ExIsActionPressed","ExIsActionReleased","ExIsMatch","ExXformedBy"]};
---
+++ docs/gdext/pr-776/godot/obj/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["NoBase"],"mod":["bounds","cap","script"],"struct":["Base","BaseMut","BaseRef","Gd","GdMut","GdRef","InstanceId","OnReady"],"trait":["Bounds","EngineBitfield","EngineEnum","GodotClass","IndexEnum","Inherits","NewAlloc","NewGd","WithBaseField"]};
