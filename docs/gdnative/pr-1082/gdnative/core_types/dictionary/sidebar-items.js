4b2c4562a4cf550edb7ec78516ec779195f186c0
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-08-21 04:34:10 +0000

    Create gdnative PR #1082 (`286a7547ec0`)
                Pull request: https://www.github.com/godot-rust/gdnative/pull/1082
                Commit:       https://www.github.com/godot-rust/gdnative/pull/1082/commits/286a7547ec0c4bc27737f11b6d136c1dbe82a4c1
                Triggered at: 2024-08-21T04:29:42Z

---
+++ docs/gdnative/pr-1082/gdnative/core_types/dictionary/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Dictionary","IntoIter","Iter"]};
---
+++ docs/gdnative/pr-1082/gdnative/tasks/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["register_runtime","set_boxed_executor","set_executor","terminate_runtime"],"struct":["Async","Context","Spawner","StaticArgs","Yield"],"trait":["AsyncMethod","StaticArgsAsyncMethod"]};
