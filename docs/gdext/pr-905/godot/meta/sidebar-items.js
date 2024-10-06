026b2fc2792a8d2fb6103020290a5a8a70c5606b
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-10-06 04:11:55 +0000

    GC closed PRs: gdext/905

--- docs/gdext/pr-905/godot/meta/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["error"],"struct":["ClassName","MethodInfo","PropertyHintInfo","PropertyInfo","RefArg","RpcConfig"],"trait":["ArrayElement","FromGodot","GodotConvert","GodotType","PackedArrayElement","ToGodot"]};
