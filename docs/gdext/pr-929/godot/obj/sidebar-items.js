bbc01221d62702afe407b20a7c05aeb374865929
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2024-10-25 18:47:35 +0000

    Create gdext PR #929 (`5b88788e6d6`)
                Pull request: https://www.github.com/godot-rust/gdext/pull/929
                Commit:       https://www.github.com/godot-rust/gdext/pull/929/commits/5b88788e6d6720f83c48e6be22ad7f24a6692775
                Triggered at: 2024-10-25T18:42:26Z

---
+++ docs/gdext/pr-929/godot/obj/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["NoBase"],"mod":["bounds","cap","script"],"struct":["Base","BaseMut","BaseRef","Gd","GdMut","GdRef","InstanceId","OnReady"],"trait":["AsObjectArg","Bounds","EngineBitfield","EngineEnum","GodotClass","IndexEnum","Inherits","NewAlloc","NewGd","WithBaseField"]};
