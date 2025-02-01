41f2f41c6facedcc09a264d37c49a2aae65f85fb
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2025-02-01 15:41:22 +0000

    Remove gdext PR #1020
            Pull request: https://www.github.com/godot-rust/gdext/pull/1020
            Triggered at: 2025-02-01T15:37:22Z

--- docs/gdext/pr-1020/godot/classes/code_edit/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["CodeCompletionKind","CodeCompletionLocation","ExAddCodeCompletionOption","ExAddCommentDelimiter","ExAddStringDelimiter","ExConfirmCodeCompletion","ExConvertIndent","ExIsInComment","ExIsInString","ExRequestCodeCompletion","ExSetCodeRegionTags"]};
