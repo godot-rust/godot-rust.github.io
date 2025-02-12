e194682ea98225805e17e02872932703d75d5a9d
Jan Haller <bromeon@gmail.com>
2025-02-12 20:35:06 +0100

    Tailwind CSS install hints

--- website/tailwind-watch.sh
+++ website/tailwind-watch.sh
@@ -4,4 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+# npm install tailwindcss@^3.0.0 @tailwindcss/cli
+
+# For upgrade to tailwind v4, see https://github.com/tailwindlabs/tailwindcss/releases/tag/v4.0.0.
+
 npx tailwindcss --watch -i src/css/main.css -o static/css/main.css
