108ff584711c8b97639d39963e3f00e95eeba24c
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2025-06-15 07:31:19 +0000

    Remove gdext PR #1201
            Pull request: https://www.github.com/godot-rust/gdext/pull/1201
            Triggered at: 2025-06-15T07:25:44Z

--- docs/gdext/pr-1201/godot/meta/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["error"],"struct":["ClassName","MethodInfo","PropertyHintInfo","PropertyInfo","RefArg"],"trait":["ArrayElement","AsArg","AsObjectArg","FromGodot","GodotConvert","GodotType","InParamTuple","OutParamTuple","PackedArrayElement","ParamTuple","ParamType","ToGodot","UniformObjectDeref"]};
